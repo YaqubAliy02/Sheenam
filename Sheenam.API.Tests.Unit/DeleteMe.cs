bb32ef35a110b04350f9059bd6b77eb0946530e4
YaqubAliy02 <yaqubaliy02@gmail.com>
2024-05-27 17:00:21 +0500

    INFRA: Initialize Tests Unit Project

--- Sheenam.API.Tests.Unit/DeleteMe.cs
+++ Sheenam.API.Tests.Unit/DeleteMe.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Sheenam.API.Tests.Unit
+﻿namespace Sheenam.API.Tests.Unit
 {
-    internal class DeleteMe
+   
+    public class DeleteMe
     {
-        
+        [Fact]
+        public void ShouldBeTrue() => Assert.True(true);
     }
 }
