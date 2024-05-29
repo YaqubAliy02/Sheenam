eaa8cf653dfdae806758bbf69045f527235f5d35
YaqubAliy02 <yaqubaliy02@gmail.com>
2024-05-29 17:30:22 +0500

    CONFIGS: Register Logging Broker

--- Sheenam/Program.cs
+++ Sheenam/Program.cs
@@ -2,6 +2,7 @@
 // Copyright (c) Coalition of Good-Hearted Engineers
 // Free To Use Comfort and Peace
 //-----------------------------
+using Sheenam.Brokers.Loggings;
 using Sheenam.Brokers.Storages;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,7 +13,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddTransient<IStorageBroker, StorageBroker>(); 
+AddBroker(builder);
 
 var app = builder.Build();
 
@@ -30,3 +31,9 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+static void AddBroker(WebApplicationBuilder builder)
+{
+    builder.Services.AddTransient<IStorageBroker, StorageBroker>();
+    builder.Services.AddTransient<ILoggingBroker, LoggingBroker>();
+}
