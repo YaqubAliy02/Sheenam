73fd499f1d45011efd495c334a3c4e89b73d5f1a
YaqubAliy02 <yaqubaliy02@gmail.com>
2024-05-31 21:33:08 +0500

    ShouldThrowCriticalDependencyExceptionOnAddIfSqlErrorOccursAndLogItAsync -> FAIL

---
+++ Sheenam.API.Tests.Unit/Services/Foundation/Guests/GuestServiceTests.Exceptions.cs
@@ -0,0 +1,51 @@
+﻿//------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Peace
+//-----------------------------
+
+using Microsoft.Data.SqlClient;
+using Moq;
+using Sheenam.Models.Foundations.Guests;
+using Sheenam.Models.Foundations.Guests.Exceptions;
+
+namespace Sheenam.API.Tests.Unit.Services.Foundation.Guests
+{
+    public partial class GuestServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnAddIfSqlErrorOccursAndLogItAsync()
+        {
+            //given
+            Guest someGuest = CreateRandomGuest();
+            SqlException sqlException = GetSqlError();
+
+            var failedGuestStorageException = new FailedGuestStorageException(sqlException);
+            var expectedGuestDependencyException =
+                new GuestDependencyException(failedGuestStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.InsertGuestAsync(someGuest))
+                .ThrowsAsync(sqlException);
+
+            //when
+            ValueTask<Guest> addGuestTask =
+                this.guestService.AddGuestAsync(someGuest);
+
+            //then
+            await Assert.ThrowsAsync<GuestDependencyException>(() =>
+            addGuestTask.AsTask());
+
+            this.storageBrokerMock.Verify(broker => 
+                broker.InsertGuestAsync(someGuest),
+                Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedGuestDependencyException))),
+                    Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
